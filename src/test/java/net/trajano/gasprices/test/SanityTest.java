fa61ea8f27ae788eb43d4337f3edf70614276c28
Archimedes Trajano <arch@trajano.net>
2012-01-23 17:38:32 -0500

    Added unit testing verification

---
+++ src/test/java/net/trajano/gasprices/test/SanityTest.java
@@ -0,0 +1,12 @@
+package net.trajano.gasprices.test;
+
+import junit.framework.TestCase;
+
+public class SanityTest extends TestCase {
+	/**
+	 * Prove the test framework works.
+	 */
+	public void testNothing() {
+
+	}
+}
