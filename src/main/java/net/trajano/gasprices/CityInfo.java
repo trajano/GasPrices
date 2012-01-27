8d19b0a456bee6f5cb2a3352bca80b0d4a503c26
Archimedes Trajano <arch@trajano.net>
2012-01-26 23:02:22 -0500

    Now provides city name

--- src/main/java/net/trajano/gasprices/CityInfo.java
+++ src/main/java/net/trajano/gasprices/CityInfo.java
@@ -14,6 +14,7 @@ public class CityInfo {
 
 	private final float currentGasPrice;
 
+	private final String name;
 	private final Date priceDate;
 	private final double priceDifference;
 	private final double priceDifferenceAbsoluteValue;
@@ -23,39 +24,41 @@ public class CityInfo {
 	private final float yesterdaysGasPrice;
 	private final boolean yesterdaysGasPriceAvailable;
 
-	public CityInfo(final JSONObject closestCityData) throws JSONException {
+	public CityInfo(final JSONObject city) throws JSONException {
 		try {
 			priceDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
-					Locale.ENGLISH).parse(closestCityData
-					.getString("price_date"));
+					Locale.ENGLISH).parse(city.getString("price_date"));
 		} catch (final ParseException e) {
 			Log.e("GasPrices", e.getMessage());
 			throw new RuntimeException(e);
 		}
-		priceDifferenceAbsoluteValue = closestCityData
-				.getDouble("price_difference");
+		name = city.getString("city_name");
+		priceDifferenceAbsoluteValue = city.getDouble("price_difference");
 		priceDifference = priceDifferenceAbsoluteValue
-				* ("-".equals(closestCityData.getString("price_prefix")) ? -1
-						: 1);
+				* ("-".equals(city.getString("price_prefix")) ? -1 : 1);
 		final Date currentDate = new Date();
 		if (priceDate.after(currentDate)) {
 			tomorrowsGasPriceAvailable = true;
 			yesterdaysGasPriceAvailable = false;
 			yesterdaysGasPrice = Float.NaN;
-			tomorrowsGasPrice = (float) closestCityData.getDouble("regular");
-			currentGasPrice = (float) (closestCityData.getDouble("regular") - priceDifference);
+			tomorrowsGasPrice = (float) city.getDouble("regular");
+			currentGasPrice = (float) (city.getDouble("regular") - priceDifference);
 		} else {
 			tomorrowsGasPriceAvailable = false;
 			yesterdaysGasPriceAvailable = true;
 			tomorrowsGasPrice = Float.NaN;
-			currentGasPrice = (float) closestCityData.getDouble("regular");
-			yesterdaysGasPrice = (float) (closestCityData.getDouble("regular") - priceDifference);
+			currentGasPrice = (float) city.getDouble("regular");
+			yesterdaysGasPrice = (float) (city.getDouble("regular") - priceDifference);
 		}
-		toStringValue = closestCityData.toString();
+		toStringValue = city.toString();
 	}
 
 	public float getCurrentGasPrice() {
 		return currentGasPrice;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public double getPriceDifference() {
