90f13652cb831b97ea935c57ac865aacd0b5cec7
Archimedes Trajano <arch@trajano.net>
2012-02-08 21:43:28 -0500

    added unit test for activity which does nothing.

---
+++ src/test/java/net/trajano/gasprices/test/GasPricesActivityTest.java
@@ -0,0 +1,16 @@
+package net.trajano.gasprices.test;
+
+import net.trajano.gasprices.GasPricesActivity;
+import android.test.ActivityUnitTestCase;
+
+public class GasPricesActivityTest extends
+		ActivityUnitTestCase<GasPricesActivity> {
+
+	public GasPricesActivityTest() {
+		super(GasPricesActivity.class);
+	}
+
+	public void testNothing() {
+	}
+
+}
